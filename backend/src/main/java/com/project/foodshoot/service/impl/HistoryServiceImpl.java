7038a8ef53dade134b96c81443c63c28426936f0
Sally Piao <46841790+1752124@users.noreply.github.com>
2020-06-17 00:55:44 +0800

    history功能完善

--- backend/src/main/java/com/project/foodshoot/service/impl/HistoryServiceImpl.java
+++ backend/src/main/java/com/project/foodshoot/service/impl/HistoryServiceImpl.java
@@ -6,6 +6,8 @@ import com.project.foodshoot.service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,7 +16,12 @@ public class HistoryServiceImpl implements HistoryService {
     @Autowired
     private HistoryMapper historyMapper;
 
-    @Override
+    public void addHistory(int historyId, int recipeId, int userId){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String visitTime = simpleDateFormat.format(new Date());
+        historyMapper.addHistory(historyId, recipeId, userId, visitTime);
+    }
+
     public List<Integer> getHistory(String userId) {
         List<History> temp = historyMapper.getAllFHistoryByUserId(userId);
         List<Integer> res = new LinkedList<>();
