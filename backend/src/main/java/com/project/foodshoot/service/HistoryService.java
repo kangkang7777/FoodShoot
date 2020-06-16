dfa28ef0f770a12cf818cc2e2349158bfcf39b34
Sally Piao <46841790+1752124@users.noreply.github.com>
2020-06-16 17:20:59 +0800

    History功能

---
+++ backend/src/main/java/com/project/foodshoot/service/HistoryService.java
@@ -0,0 +1,11 @@
+package com.project.foodshoot.service;
+
+import com.project.foodshoot.entity.History;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface HistoryService {
+    List<Integer> getHistory(String userId);
+}
